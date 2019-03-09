a71e03ed075e67cb9639675e771974046f5cf0c4
indionapolis <panddromas@gmail.com>
2019-03-09 15:05:26 +0300

    working node

--- week7/node.c
+++ week7/node.c
@@ -38,14 +38,14 @@ map_t data_hashmap;
 
 
 /*Server process is running on this port no. Client has to send data to this port no*/
-#define SERVER_PORT     2000
+#define SERVER_PORT     2001
 
-#define DEST_PORT            2000
+#define DEST_PORT            2001
 #define SERVER_IP_ADDRESS   "10.91.45.222"
 
 test_struct_t client_data;
 test_struct_t test_struct;
-result_struct_t result;
+
 result_struct_t res_struct;
 char data_buffer[1024];
 
@@ -148,7 +148,7 @@ void server(){
         FD_ZERO(&readfds);                     /* Initialize the file descriptor set*/
         FD_SET(master_sock_tcp_fd, &readfds);  /*Add the socket to this set on which our server is running*/
 
-        printf("SERVER: blocked on select System call...\n");
+        printf("\nSERVER: blocked on select System call... waits for connection\n\n");
 
 
 
@@ -159,7 +159,7 @@ void server(){
         /*Some data on some fd present in set has arrived, Now check on which File descriptor the data arrives, and process accordingly*/
         if (FD_ISSET(master_sock_tcp_fd, &readfds)) {
             /*Data arrives on Master socket only when new client connects with the server (that is, 'connect' call is invoked on client side)*/
-            printf("SERVER: New connection recieved recvd, accept the connection. \nSERVER: Client and Server completes TCP-3 way handshake at this point\n");
+            printf("SERVER: New connection received, accept the connection. \nSERVER: Client and Server completes TCP-3 way handshake at this point\n");
 
 
             comm_socket_fd = accept(master_sock_tcp_fd, (struct sockaddr *) &client_addr, (socklen_t *) &addr_len);
@@ -176,7 +176,8 @@ void server(){
             value->client_addr = client_addr;
 
             char * key = malloc(sizeof(char)*15);
-            sprintf(key, "%s:%u\n", inet_ntoa(client_addr.sin_addr));
+
+            sprintf(key, "%s:%u\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
             hashmap_put(data_hashmap, key, value);
 
@@ -189,7 +190,7 @@ void server(){
 
 
 void client(){
-    sleep(5);
+    sleep(3);
     int sockfd = 0, sent_recv_bytes = 0;
 
     int addr_len = 0;
@@ -213,9 +214,14 @@ void client(){
 
 
     while(1) {
+        result_struct_t result;
+
 //        wait for server ping
         recvfrom(sockfd, (char *)&result, sizeof(result_struct_t), 0,
                  (struct sockaddr *)&dest, (socklen_t *) &addr_len);
+
+
+
         printf("CLIENT: recv from client %s:%u\n",
                inet_ntoa(dest.sin_addr), ntohs(dest.sin_port));
 
@@ -239,9 +245,13 @@ void client(){
 }
 
 int main(int argc, char **argv) {
-    pthread_t threads[1];
-    pthread_create(&threads[0], NULL, (void *)server, NULL);
-    client();
+    pthread_t threads[5];
+    pthread_create(&threads[0], NULL, (void *)client, NULL);
+    pthread_create(&threads[1], NULL, (void *)client, NULL);
+    pthread_create(&threads[2], NULL, (void *)client, NULL);
+    pthread_create(&threads[3], NULL, (void *)client, NULL);
+    pthread_create(&threads[4], NULL, (void *)client, NULL);
+    server();
     return 0;
 }
 
